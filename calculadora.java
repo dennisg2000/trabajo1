657516daa2dc5c6f0136dcaecde7182aad04d76b
JamesCordova <jcordovas@unsa.edu.pe>
2022-05-05 13:55:47 -0500

    Metodo del modulo definido

--- calculadora.java
+++ calculadora.java
@@ -3,5 +3,7 @@ class Calculator {
   int sub(int a, int b){ return 0; }
   int mul(int a, int b){ return 0; }
   int div(int a, int b){ return 0; }
-  int mod(int a, int b){ return 0; }
+  int mod(int a, int b){ 
+	  return a % b; 
+  }
 }
